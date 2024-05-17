accb2de3d120d94ba249cb198723373f194c9a88
kamranifti <kamranifti030@gmail.com>
2024-05-17 11:15:34 +0500

    second push

--- home.php
+++ home.php
@@ -1,3 +1,14 @@
+<?php 
+session_start();
+
+if(!isset($_SESSION["user_email"])){
+
+  header("location: index.php");
+  
+  }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
--- index.php
+++ index.php
@@ -83,7 +83,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
               <div class="d-flex justify-content-center py-4">
                 <a href="index.html" class="logo d-flex align-items-center w-auto">
-                  <!-- <img src="assets/img/logo.png" alt=""> -->
                   <span class="d-none d-lg-block"><i class="fa-solid fa-dog"></i> PetHouse</span>
                 </a>
               </div><!-- End Logo -->
@@ -94,7 +93,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
                   <div class="pt-4 pb-2">
                     <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
-                    <p class="text-center small">Enter your username & password to login</p>
+                    <p class="text-center small">Enter your Email & Password to login</p>
                   </div>
 
                   <form class="row g-3 needs-validation" novalidate method="POST">
